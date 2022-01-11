3d436b7a4ed810692c4015b9f020c363b75f5253
Anthony Loera <anthony.loera@logicloud.tech>
2022-01-11 10:06:56 -0800

    Added prepush hook

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 export interface Post {
-    id: number
-    title: string
-    body: string
+  id: number;
+  title: string;
+  body: string;
 }
 
 export async function getPosts() {
-  const url = "https://jsonplaceholder.typicode.com/posts";
+  const url = 'https://jsonplaceholder.typicode.com/posts';
   const { data } = await axios.get<Post[]>(url);
   return data;
 }
